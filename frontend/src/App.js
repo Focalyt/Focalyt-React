5586a6be780ff75bf03dab64d3c1bec3a7a360ce
Akash Gaurav <akash.gauranv@focalyt.com>
2025-03-01 18:34:56 +0530

    new pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 
-
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import HomePage from '../src/Pages/Front/HomePage/HomePage';
 import About from './Pages/Front/About/About';
@@ -13,7 +13,8 @@ import CompanyLogin from './Component/Layouts/App/Company/CompanyLogin';
 import Community from './Pages/Front/Community/Community';
 import CandidateLayout from './Component/Layouts/App/Candidates';
 import CandidateDashboard from './Pages/App/Candidate/CandidateDashboard/CandidateDashboard';
-
+import CandidateProfile from './Pages/App/Candidate/CandidateProfile/CandidateProfile';
+import CandidatesDocuments from "./Pages/App/Candidate/CandidateDocuments/CandidateDocumets"
 
 const Layout = () => {
   const location = useLocation(); 
@@ -31,7 +32,13 @@ const Layout = () => {
         <Route exact path="/coursedetails/:courseId" element={<CourseDetails />} />
         <Route exact path="/company/login" element={<CompanyLogin />} />
         <Route exact path="/community" element={<Community />} />
-        <Route exact={true} path ="/candidate/dashboard" element={<CandidateDashboard />}/>
+
+        {/* Candidate Parent Route */}
+        <Route path="/candidate" element={<CandidateLayout />}>
+                <Route path="dashboard" element={<CandidateDashboard />} />
+                <Route path ="myprofile" element={<CandidateProfile/>}/>
+                <Route path ="document" element={<CandidatesDocuments/>}/>
+            </Route>
       </Routes>
 
 
