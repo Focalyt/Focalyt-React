dc401dc4d900821d2e30a527922bbfd8787c7c63
Akash Gaurav <akash.gaurav@focalyt.com>
2025-07-16 16:08:18 +0530

    add counceller name in adminssion list (3791 ,3884 )

--- frontend/src/Pages/App/College/Course/AdmissionList.jsx
+++ frontend/src/Pages/App/College/Course/AdmissionList.jsx
@@ -3788,7 +3788,7 @@ const AdmissionList = () => {
                                                     </div>
                                                     <div className="info-group">
                                                       <div className="info-label">Counsellor Name</div>
-                                                      <div className="info-value">{profile._course?.counslername}</div>
+                                                      <div className="info-value">{profile.leadAssignment[profile.leadAssignment.length - 1]?.counsellorName || 'N/A'}</div>
                                                     </div>
                                                   </div>
                                                 </div>
@@ -3882,7 +3882,7 @@ const AdmissionList = () => {
                                                   <div className="col-xl- col-3">
                                                     <div className="info-group">
                                                       <div className="info-label">Counsellor Name</div>
-                                                      <div className="info-value">{profile._course?.counslername}</div>
+                                                      <div className="info-value">{profile.leadAssignment[profile.leadAssignment.length - 1]?.counsellorName || 'N/A'}</div>
                                                     </div>
                                                   </div>
                                                   <div className="col-xl- col-3">
